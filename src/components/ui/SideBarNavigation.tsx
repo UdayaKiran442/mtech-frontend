ac3e99054055d565b6e502a43354e15f8c250063
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2026-05-07 16:52:32 +0530

    fix: sidebar navigation, active view UI

--- src/components/ui/SideBarNavigation.tsx
+++ src/components/ui/SideBarNavigation.tsx
@@ -9,7 +9,7 @@ type ISideBarNavigationProps = {
 export function SideBarNavigation({children, label, view}: ISideBarNavigationProps) {
     return (
         <div>
-            <div className={`flex gap-3 items-center mt-6 p-2 cursor-pointer ${label === view ? "bg-bg_secondary py-2 rounded-2xl" : "hover:bg-bg_secondary hover:py-2 hover:rounded-2xl"}`}>
+            <div className={`flex gap-3 items-center mt-6 p-2 cursor-pointer ${label === view ? "bg-bg_secondary py-2 border-l-2 border-[#00F0FF]" : "hover:bg-bg_secondary hover:py-2 hover:border-l-2 hover:border-[#00F0FF]"}`}>
                 {children}
                 <p className={`font-semibold text-text ${label === view ? "font-bold" : ""} `}>{label}</p>
             </div>
